961101acae59394ebcd818cc11e9d44fec2ba8dd
johanseo <trullo393@gmail.com>
2019-11-23 17:22:23 +0900

    (테스트)HttpRequestCHSUtil 추가 및 webapp 디렉터리의 해당파일 열기

--- src/main/java/util/HtppRequestCHSUtil.java
+++ src/main/java/util/HtppRequestCHSUtil.java
@@ -1,5 +1,7 @@
 package util;
 
+import model.User;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,5 +13,11 @@ public class HtppRequestCHSUtil {
             url = matcher.group(0);
         }
         return url.trim();
+    }
+
+    public User userDefaultSetting(String lineToken) {
+
+
+        return new User("1","2","A","B");
     }
 }
